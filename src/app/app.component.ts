5836969d751631d4ca0b578b49834fed3b54d528
surendra190 <surendrachaudhary19@gmail.com>
2022-03-10 17:36:14 +0530

    Json-Server code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Json-server';
+data={
+  "id": 4,
+  "title": "json-server new",
+  "author": "typicode34"
+}
+
+
+new:any;
+constructor(private _http:HttpClient){
+this.postdata(this.data);
+this.getdata();
+}
+
+  postdata(data:any){
+return this._http.post<any>('http://localhost:3000/posts',data).subscribe((res)=>{
+  return res;
+},(err)=>{
+  alert(" post "+JSON.stringify(err));
+})
+  }
+
+
+  getdata(){
+    return this._http.get<any>('http://localhost:3000/posts').subscribe((res)=>{
+      this.new= res;
+    },(err)=>{
+      alert(" get "+JSON.stringify(err)+" get");
+    })
+      }
+    
+
+  
+
+
 }
